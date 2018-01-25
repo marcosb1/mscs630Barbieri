6c8551a4fe042aefac790698434d52d512b4eea9
marcosb1 <Marcos.Barbieri1@marist.edu>
2018-01-25 11:16:53 -0500

    part 1 of lab 2 completed

---
+++ labs/2/Driver.java
@@ -0,0 +1,81 @@
+/**
+ * file: Driver.java
+ * author: Marcos Barbieri
+ * course: MSCS 630L
+ * assignment: lab 2
+ * due date: 
+ * version: 1.0
+ *
+ *
+ * TODO: Description here
+ */
+
+import java.util.Scanner;
+
+/**
+ */
+class Driver {
+
+  /**
+   * euclidAlg
+   * 
+   * This function recursively finds the Euclidian GCD based on
+   * the Euclidian GCD theorem/algorithm
+   * This works through the following:
+   *        a     = q_1   * b     + r_1
+   *        b     = q_2   * r_1   + r_2
+   *        r_1   = q_3   * r_2   + r_3
+   *              ---
+   *        r_n-2 = q_n   * r_n-1 + r_n
+   *        r_n-1 = q_n+1 * r_n   + 0
+   *  Where r_n is our GCD
+   * 
+   * parameters:
+   *  a: long containing the first number (should be greater than b)
+   *  b: long containing the second number
+   *
+   * return: 
+   *  long which is the greatest common denominator
+   */
+  public static long euclidAlg(long a, long b) {
+    // we want to make sure that a >= b, if not swap them
+    if (a < b) {
+      long tempA = a;
+      a = b;
+      b = tempA;
+    }    
+
+    // this is how we check if we are done with the recursion
+    long r = a % b;
+
+    // when the remainder is 0 simply return b, according to Euclidean GCD
+    if (r == 0) {
+      return b;
+    }
+    
+    return euclidAlg(b, r);
+  }
+
+  /**
+   * main
+   * 
+   * The main method will take in text strings containing numbers
+   * for which we will take each line, split by space and compute 
+   * the Euclidian GCD 
+   *
+   * parameters:
+   *  args: array of String containing suplemental cmd arguments
+   *    (not used in this program)
+   *
+   * return: 
+   */
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    while (input.hasNext()) {
+      String plainText = input.nextLine();
+      String[] splitted = plainText.split("\\s+");
+      long gcd = euclidAlg(Long.parseLong(splitted[0]), Long.parseLong(splitted[1]));
+      System.out.println(gcd); 
+    }
+  }
+}
