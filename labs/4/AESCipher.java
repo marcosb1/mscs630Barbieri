c6000bf933813e2f32c299343fc6a01653263d0e
marcosb1 <Marcos.Barbieri1@marist.edu>
2018-04-02 22:09:03 -0400

    Lab 4 skeleton setup

---
+++ labs/4/AESCipher.java
@@ -0,0 +1,25 @@
+/**
+ * file: AESCipher.java
+ * author: Marcos Barbieri
+ * course: MSCS 630L
+ * assignment: lab 4 
+ * due date: 04/03/2018
+ * version: 0.1
+ *
+ *
+ */
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class AESCipher {
+
+  public static String[] aesRoundKeys(String KeyHex) {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ labs/4/DriverAES.java
@@ -0,0 +1,23 @@
+/**
+ * file: DriverAES.java
+ * author: Marcos Barbieri
+ * course: MSCS 630L
+ * assignment: lab 
+ * due date: 04/03/2018
+ * version: 0.1
+ *
+ *
+ */
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class DriverAES {
+
+  
+
+  public static void main(String[] args) {
+
+  }
+
+}
