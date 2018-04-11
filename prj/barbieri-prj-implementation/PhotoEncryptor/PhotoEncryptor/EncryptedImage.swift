a014ba4c4f33d4213fc66596cb925d2805557216
marcosb1 <Marcos.Barbieri1@marist.edu>
2018-04-11 00:22:01 -0400

    milestone update (document is longer than three pages but thats because its double spaced, easier to read)

--- prj/barbieri-prj-implementation/PhotoEncryptor/PhotoEncryptor/EncryptedImage.swift
+++ prj/barbieri-prj-implementation/PhotoEncryptor/PhotoEncryptor/EncryptedImage.swift
@@ -158,7 +158,6 @@ class ImageEncryptor {
             return nil
         }
         
-        let returnImg = UIImage.init(CGImage: newImg)
-        return returnImg
+        return UIImage.init(cgImage: newImg)
     }
 }
